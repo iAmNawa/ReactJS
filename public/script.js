23ca3d927352f81b10daec8ac84fa311f3fbb49e
Paul Borawski <paulborawski14@gmail.com>
2016-07-11 08:31:08 -0700

    simplest possible react app

---
+++ public/script.js
@@ -0,0 +1,12 @@
+var MyComponent = React.createClass({
+    render: function() {
+        return <div>My React Component</div>;
+    }
+});
+
+React.render( <div>
+    <MyComponent />
+            <MyComponent />
+            <MyComponent />
+            </div>
+    , document.getElementById('react-container'));
