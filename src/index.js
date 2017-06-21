55f709ae7a5977f77b5ddbc368017c7371a6cdf4
Paul Borawski <paulborawski14@gmail.com>
2017-06-21 12:11:58 -0700

    adding a component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from 'search_bar';
+
+const API_KEY = '#';
+
 // Create a new component. This component should produce some HTML
 
 const App = function() {
